0850c1991a52470be1cd5c722a873dd0e3b4ad39
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-12-19 21:11:17 +0100

    assertions

--- mtt.lua
+++ mtt.lua
@@ -21,16 +21,28 @@ local pos = { x=0, y=0, z=0 }
 mtt.emerge_area(pos, pos)
 mtt.register("serialize and deserialize chunk", function(callback)
 
+    -- vars
     local chunk_pos = {x=0, y=0, z=0}
     local filename = minetest.get_worldpath() .. "/chunk.zip"
+    local node_pos = { x=10, y=5, z=0 }
+
+    -- prepare
+    minetest.set_node(node_pos, { name = "default:stone", param2 = 66 })
 
     local success, err_msg = mapsync.serialize_chunk(chunk_pos, filename)
     assert(success)
     assert(not err_msg)
 
-    success, err_msg = mapsync.deserialize_chunk(chunk_pos, filename)
+    local target_chunk_pos = vector.add(chunk_pos, { x=1, y=0, z=0 })
+    success, err_msg = mapsync.deserialize_chunk(target_chunk_pos, filename)
     assert(success)
     assert(not err_msg)
+
+    -- assert
+    local target_node_pos = vector.add(node_pos, { x=80, y=0, z=0 })
+    local node = minetest.get_node(target_node_pos)
+    assert(node.name == "default:stone")
+    assert(node.param2 == 66)
 
     callback()
 end)
