af2b82f36547986dcee147ef8925f8b105ad4bfe
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-01-18 22:04:57 +0100

    mapblock conditions

--- create_diff.lua
+++ create_diff.lua
@@ -12,6 +12,16 @@ function mapsync.create_diff(baseline_chunk, chunk_pos)
                 local blockdata = mapsync.serialize_mapblock(mapblock_pos, node_mapping)
                 local baseline_mapblock = baseline_chunk.mapblocks[minetest.pos_to_string(rel_mapblock_pos)]
 
+                if blockdata.empty and baseline_mapblock then
+                    -- block removed
+                elseif not blockdata.empty and not baseline_mapblock then
+                    -- block added
+                elseif blockdata.empty and not baseline_mapblock then
+                    -- nothing here, nothing changed
+                else
+                    -- both blocks exist, compare
+                end
+
                 print(dump({
                     rel_mapblock_pos = rel_mapblock_pos
                 }))
