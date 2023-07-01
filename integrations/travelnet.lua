878a0a55dca9c893212f4304173ae88a21b71fa9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-07-01 18:52:53 +0200

    travelnet emerge

--- integrations/travelnet.lua
+++ integrations/travelnet.lua
@@ -20,3 +20,25 @@ function travelnet.set_travelnets(playername, travelnets)
         return old_set_travelnets(playername, travelnets)
     end
 end
+
+-- ${playername} / ${station_name} => true
+local emerged_entries = {}
+
+-- emerge travelnet destinations before the player visits them
+local old_show_current_formspec = travelnet.show_current_formspec
+function travelnet.show_current_formspec(pos, meta, playername)
+    local travelnets = travelnet.get_travelnets(playername)
+    local station_network = meta:get_string("station_network")
+
+    local key = playername .. "/" .. station_network
+    if not emerged_entries[key] then
+        if travelnets and travelnets[station_network] then
+            for _, entry in pairs(travelnets[station_network]) do
+                minetest.emerge_area(entry.pos, entry.pos)
+            end
+        end
+        emerged_entries[key] = true
+    end
+
+    return old_show_current_formspec(pos, meta, playername)
+end
