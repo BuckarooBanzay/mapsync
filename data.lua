ac6a7265c04f394f2259e3b68f9ecaa54f02aef5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-20 08:48:41 +0200

    nil-check on data backend

--- data.lua
+++ data.lua
@@ -1,6 +1,11 @@
 
 function mapsync.save_data(key, value)
     local data_backend_def = mapsync.get_data_backend()
+    if not data_backend_def then
+        -- no data backend defined
+        return
+    end
+
     local data_handler = mapsync.select_data_handler(data_backend_def)
     if data_handler then
         data_handler.save_data(data_backend_def, key, value)
@@ -9,6 +14,11 @@ end
 
 function mapsync.load_data(key)
     local data_backend_def = mapsync.get_data_backend()
+    if not data_backend_def then
+        -- no data backend defined
+        return
+    end
+
     local data_handler = mapsync.select_data_handler(data_backend_def)
     if data_handler then
         return data_handler.load_data(data_backend_def, key)
