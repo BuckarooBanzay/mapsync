bc8a88b8b759deaa452ff287bd8ab71db312807c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-01-11 08:28:36 +0100

    encryption benchmark

--- crypto.spec.lua
+++ crypto.spec.lua
@@ -23,3 +23,23 @@ mtt.register("mapsync.encrypt > 20 bytes", function(callback)
     assert(plaintext == plaintext2)
     callback()
 end)
+
+mtt.register("mapsync.encrypt benchmark", function(callback)
+    local key = "mykey"
+    local t = {}
+    for _=1,1000*1000 do
+        table.insert(t, "x")
+    end
+    local plaintext = table.concat(t)
+
+    local t1 = minetest.get_us_time()
+    local encrypted = mapsync.encrypt(key, plaintext)
+    local t2 = minetest.get_us_time()
+    print("encryption of " .. #plaintext .. " bytes took " .. (t2 - t1) .. " us")
+    -- make sure 1MB is en-/decrypted in under 100 ms
+    assert( (t2 - t1) < 100*1000)
+
+    assert(plaintext ~= encrypted)
+    assert(#plaintext == #encrypted)
+    callback()
+end)
