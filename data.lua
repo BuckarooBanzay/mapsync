dbf2a1b4987ddeb6b5a735d633093c94a3e18723
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-08-12 06:58:18 +0200

    add `mapsync.get_data_file` and tests

--- data.lua
+++ data.lua
@@ -1,5 +1,6 @@
 local global_env = ...
 
+-- save simple key-value data (tables, strings, etc)
 function mapsync.save_data(key, value)
     local data_backend_def = mapsync.get_data_backend()
     if not data_backend_def then
@@ -12,6 +13,8 @@ function mapsync.save_data(key, value)
     f:close()
 end
 
+-- load simple key-value data
+-- returns nil if not available or not existing
 function mapsync.load_data(key)
     local data_backend_def = mapsync.get_data_backend()
     if not data_backend_def then
@@ -26,4 +29,18 @@ function mapsync.load_data(key)
     local value = minetest.deserialize(f:read("*all"))
     f:close()
     return value
+end
+
+-- returns a file to write to in the data-storage, nil if not available
+function mapsync.get_data_file(key, mode)
+    -- default to read
+    mode = mode or "r"
+
+    local data_backend_def = mapsync.get_data_backend()
+    if not data_backend_def then
+        -- no data backend defined
+        return
+    end
+
+    return global_env.io.open(data_backend_def.path .. "/" .. key, mode)
 end
--- data.spec.lua
+++ data.spec.lua
@@ -15,6 +15,19 @@ mtt.register("data", function(callback)
     assert(value.y == 1)
 
     assert(not mapsync.load_data("y"))
+    
+    -- data file
+    assert(mapsync.get_data_file("myfile.txt")) -- defaults to read-mode
+    assert(mapsync.get_data_file("myfile2.txt", "w"))
+    assert(not mapsync.get_data_file("myfile-non-existent-file.txt", "r"))
+
+    -- write to data file
+    assert(not mapsync.get_data_file("myfile3.txt"))
+    local f = mapsync.get_data_file("myfile3.txt", "w")
+    assert(f)
+    f:write("stuff")
+    f:close()
+    assert(mapsync.get_data_file("myfile3.txt"))
 
     callback()
 end)
